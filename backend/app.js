4e4c5cb7d3cd95af2194437e0b38ffa5c36afec4
SARB <mrsinghbusiness05@gmail.com>
2024-05-17 06:02:08 +0530

    adding another env

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended:false}));
 
 // add cors
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : process.env.FRONTEND_URL,
     credentials :true,
 }))
 
